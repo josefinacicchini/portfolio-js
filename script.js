7da786ce96de1e4ecd5bf292ec9550004aa0a329
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2025-05-04 21:26:43 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () { 
+    let button = document.getElementById("button");
+    let input = document.getElementById("input");
+    let output = document.getElementById("output");
+    let result = document.getElementById("result");
+    let error = document.getElementById("error");
+    
+    button.addEventListener("click", function () {
+        alert("Formulario enviado");
+}
